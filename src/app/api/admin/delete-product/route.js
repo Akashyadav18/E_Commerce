79c747e34df39cd2a7f782c8a0bbdfeeba20a11d
Akashyadav18 <shadowlee5469@gmail.com>
2023-12-25 19:08:45 +0530

    route delete admin product

--- src/app/api/admin/delete-product/route.js
+++ src/app/api/admin/delete-product/route.js
@@ -10,13 +10,13 @@ export async function DELETE (req) {
         const {searchParams} = new URL(req.url);
         const id = searchParams.get('id');
         if(!id) {
-            return NextResponse,json({message: 'Product Id Required', status: 500, success: false})
+            return NextResponse.json({message: 'Product Id Required', status: 500, success: false})
         }
         const deleteProduct = await product.findByIdAndDelete(id);
         if(deleteProduct) {
-            return NextResponse,json({message: 'Product Delete Successfully', status: 200, success: true});
+            return NextResponse.json({message: 'Product Delete Successfully', status: 200, success: true});
         } else {
-            return NextResponse,json({message: 'Fail to Delete Product', status: 500, success: false});
+            return NextResponse.json({message: 'Fail to Delete Product', status: 500, success: false});
         }
     } catch (error) {
         console.log(error);
--- src/components/CommonListing/ProductButtons/index.jsx
+++ src/components/CommonListing/ProductButtons/index.jsx
@@ -1,15 +1,31 @@
 "use client"
 
+import ComponentLevelLoader from '@/components/Loader/componentLevel';
 import { GlobalContext } from '@/context/Index';
+import { deleteAProduct } from '@/services/product';
 import { usePathname, useRouter } from 'next/navigation'
 import React, { useContext } from 'react'
+import toast from 'react-hot-toast';
 
 const ProductButton = ({ item }) => {
 
   const pathName = usePathname();
-  const { setCurrentUpdatedProduct } = useContext(GlobalContext);
+  const { setCurrentUpdatedProduct, setComponentLevelLoader, componentLevelLoader } = useContext(GlobalContext);
   const router = useRouter()
   const isAdminView = pathName.includes('admin-view');
+
+  const handleDeleteProduct = async (item) => {
+    setComponentLevelLoader({loading: true, id: item._id});
+    const res = await deleteAProduct(item._id);
+    if(res.success) {
+      setComponentLevelLoader({loading: false, id: ""});
+      toast.success(res.message, {position: "top-center"});
+      router.refresh(); 
+    } else {
+      toast.error(res.message, {position: "top-center"});
+      setComponentLevelLoader({loading: false, id: ""});
+    }
+  }
 
   return isAdminView ? (
     <>
@@ -20,8 +36,14 @@ const ProductButton = ({ item }) => {
         }}
         className='mt-1.5 flex w-full justify-center bg-black px-5 py-3 font-medium uppercase tracking-wide text-white'>
         Update</button>
-      <button onClick={() => console.log("Delete")} className='mt-1.5 flex w-full justify-center bg-black px-5 py-3 font-medium uppercase tracking-wide text-white'>
-        Delete</button>
+      <button onClick={() => handleDeleteProduct(item)} className='mt-1.5 flex w-full justify-center bg-black px-5 py-3 font-medium uppercase tracking-wide text-white'>
+      {
+        componentLevelLoader && componentLevelLoader.loading && item._id === componentLevelLoader.id ? 
+        <ComponentLevelLoader 
+          text={"Deleting"} color={"#ffffff"} loading={componentLevelLoader && componentLevelLoader.loading}
+        /> : "DELETE"
+      }
+      </button>
     </>
   ) : (
     <>
