9d5b2e163455f9ab31fbfe82b60f28452616d050
Akashyadav18 <shadowlee5469@gmail.com>
2024-01-17 10:01:37 +0530

    checkout UI

--- src/components/CartModel/index.jsx
+++ src/components/CartModel/index.jsx
@@ -79,7 +79,7 @@ const CartModel = () => {
           <Fragment>
             <div className='flex justify-evenly items-center gap-5'>
               <button type='button' onClick={() => {router.push("/cart"), setShowCartModal(false)}} className='w-full inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide'>Go To Cart</button>
-              <button disabled={cartItems && cartItems.length === 0} type='button' className='w-full inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide disabled:opacity-50'>Check Out</button>
+              <button onClick={() => {router.push("/checkout"), setShowCartModal(false)}} disabled={cartItems && cartItems.length === 0} type='button' className='w-full inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide disabled:opacity-50'>Check Out</button>
             </div>
             <div className='mt-6 flex justify-center text-center text-sm text-gray-600'>
               <button type='button' className='font-medium text-gray'>Continue Shopping <span aria-hidden="true">&rarr;</span></button>
