c84dd7f7e968a8a433a4c753c659402dc4f02fb2
Akashyadav18 <shadowlee5469@gmail.com>
2024-01-25 19:44:58 +0530

    order loader

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -4,29 +4,43 @@ import { GlobalContext } from "@/context/Index"
 import { getAllOrdersForUser } from "@/services/order";
 import { useContext, useEffect } from "react"
 import toast from "react-hot-toast";
+import { PulseLoader } from "react-spinners";
 
-export default function Orders () {
+export default function Orders() {
 
-    const {user, pageLevelLoader, setPageLevelLoader, allOrdersForUser, setAllOrdersForUser} = useContext(GlobalContext);
+    const { user, pageLevelLoader, setPageLevelLoader, allOrdersForUser, setAllOrdersForUser } = useContext(GlobalContext);
 
-    async function extractAllOrders () {
+    async function extractAllOrders() {
         setPageLevelLoader(true);
         const res = await getAllOrdersForUser(user?._id);
-        if(res.success) {
+        if (res.success) {
             setPageLevelLoader(false);
             setAllOrdersForUser(res.data);
-            toast.success(res.message, {position: "top-center"});
+            toast.success(res.message, { position: "top-center" });
         } else {
             setPageLevelLoader(false);
-            toast.success(res.message, {position: "top-center"});
+            toast.success(res.message, { position: "top-center" });
         }
     }
 
     useEffect(() => {
-        if(user !== null) extractAllOrders();
+        if (user !== null) extractAllOrders();
     }, [user]);
 
-    console.log("AllOrders :",allOrdersForUser);
+    console.log("AllOrders :", allOrdersForUser);
+
+    if (pageLevelLoader) {
+        return (
+            <div className="w-full min-h-screen flex justify-center items-center">
+                <PulseLoader
+                    color={"#000000"}
+                    loading={pageLevelLoader}
+                    size={30}
+                    data-testid="loader"
+                />
+            </div>
+        )
+    }
 
     return (
         <section>Your all Orders</section>
