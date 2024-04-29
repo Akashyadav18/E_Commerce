b1e63af78df338f5bcbc5c02c7a170ba2d8d3247
Akashyadav18 <shadowlee5469@gmail.com>
2024-04-29 15:43:22 +0530

    cors changes

--- next.config.js
+++ next.config.js
@@ -6,15 +6,17 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/api/:path*", // Matches all API routes
+                // matching all API routes
+                source: "/api/:path*",
                 headers: [
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // Allow all origins during development (replace with specific origin for production)
-                    { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" }, // Common HTTP methods
-                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }, // Allow necessary headers for data and authentication
-                ],
-            },
-        ];
-    },
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
